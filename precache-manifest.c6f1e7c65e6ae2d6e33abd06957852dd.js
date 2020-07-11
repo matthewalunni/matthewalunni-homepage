3c99f65d378faa97e292417130f244a9d4d2a530
matthewalunni <matthew.alunni@gmail.com>
2020-07-11 13:20:52 -0400

    Updates

---
+++ precache-manifest.c6f1e7c65e6ae2d6e33abd06957852dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb896cff5f5ebd3a68b4c219653b720",
+    "url": "/matthewalunni-homepage/index.html"
+  },
+  {
+    "revision": "fc543be8c516352bcb43",
+    "url": "/matthewalunni-homepage/static/css/main.101f798c.chunk.css"
+  },
+  {
+    "revision": "aee891e8fa335af2bbb8",
+    "url": "/matthewalunni-homepage/static/js/2.3b3db7f4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/matthewalunni-homepage/static/js/2.3b3db7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc543be8c516352bcb43",
+    "url": "/matthewalunni-homepage/static/js/main.a62d11da.chunk.js"
+  },
+  {
+    "revision": "bbee792165c9afd39141",
+    "url": "/matthewalunni-homepage/static/js/runtime-main.b4a2e0f0.js"
+  },
+  {
+    "revision": "eef2bc551522f364c8f3bbc800bb756e",
+    "url": "/matthewalunni-homepage/static/media/jumbotron-home (2).eef2bc55.jpg"
+  },
+  {
+    "revision": "40376fd9e1308ddfefb5e2af74b039e7",
+    "url": "/matthewalunni-homepage/static/media/jumbotron-projects.40376fd9.jpg"
+  }
+]);
